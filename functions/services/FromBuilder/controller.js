51c4f24262a6ce7dccd9ec47f9982b781a345541
Praveenkusuluri08 <praveenkusuluri08@gmail.com>
2022-04-08 21:32:17 +0530

    Dynamic Form templates backend endpoint

--- functions/services/FromBuilder/controller.js
+++ functions/services/FromBuilder/controller.js
@@ -3,16 +3,33 @@ const FormBuilder = require("./models");
 
 const router = require("express").Router();
 
-router.post("/create-template",closedEnd,(req,res)=>{
-    const inputs = req.body
-    const obj = new FormBuilder(req.user)
-    obj._createTemplates(inputs).then(()=>{
-        return res.status(200).json({message:"Template updated successfully"})
+router.post("/create-template", closedEnd, (req, res) => {
+  const inputs = req.body;
+  const obj = new FormBuilder(req.user);
+  obj
+    ._createTemplates(inputs)
+    .then(() => {
+      return res.status(200).json({ message: "Template updated successfully" });
     })
-    .catch((err)=>{
-        console.log(err)
-        return res.status(400).json({error:"Failed to update template"})
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json({ error: "Failed to update template" });
+    });
+});
 
-module.exports=router
+router.post("/create-formBuilder", closedEnd, (req, res) => {
+  const inputs = req.body;
+  const obj = new FormBuilder(req.user);
+
+  obj
+    ._createTemplates(inputs)
+    .then(() => {
+      return res.status(200).json({ message: "Forms created successfully" });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(400).json({ error: "Failed to create form templates" });
+    });
+});
+
+module.exports = router;
--- functions/services/FromBuilder/models.js
+++ functions/services/FromBuilder/models.js
@@ -2,22 +2,38 @@ const { db } = require("../utils/admin");
 
 class FormBuilder {
   constructor(user) {
-    console.log(user)
+    console.log(user);
     this.actionPerformer = user;
   }
   async _createTemplates(inputs) {
-    const dbRef = db.collection("FORM_BUILDER").doc(this.actionPerformer.uid+"-FORM");
+    const dbRef = db
+      .collection("FORM_BUILDER")
+      .doc(this.actionPerformer.uid + "-FORM");
     return dbRef.set(
       {
         ...inputs,
         creatdAt: new Date().toISOString(),
         id: "Employee-profile-template",
-        isExists:true
+        isExists: true,
       },
       { merge: true }
     );
   }
+  async _createFormTemplates(inputs) {
+    const { documentName, collectionName } = inputs;
 
+    const dbRef = db.collection(collectionName).doc(documentName);
+    let documentData = {};
+    Object.entries(inputs).forEach(([key, value]) => {
+      if (key === documentName || key === collectionName)
+        documentData[key] = value;
+    });
+    return dbRef.set({
+      ...documentData,
+      isExists: true,
+      cretedAt: new Date().toISOString(),
+    },{merge: true});
+  }
 }
 
 module.exports = FormBuilder;
