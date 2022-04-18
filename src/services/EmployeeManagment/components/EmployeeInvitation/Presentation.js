216a81e1529adad87bf8bb8daa2b2fe65d9f346b
Lokesh <86008366+Lokesh5g2@users.noreply.github.com>
2022-04-18 15:13:25 +0530

    updatedEmplyeeInvitation

--- src/services/EmployeeManagment/components/EmployeeInvitation/Presentation.js
+++ src/services/EmployeeManagment/components/EmployeeInvitation/Presentation.js
@@ -3,51 +3,6 @@ import Button from '@material-ui/core/Button';
 import {
     TextField, Card, CardContent, CardActions
 } from '@material-ui/core';
-// function Presentation(props) {
-//     const { isOpen, onOpen, onClose } = useDisclosure()
-//     return (
-//         <div>
-
-//             <Button onClick={onOpen}>Invite Employee  </Button>
-
-//             <Modal isOpen={isOpen} onClose={onClose} >
-//                 <ModalOverlay />
-//                 <ModalContent>
-//                     <form >
-//                         <ModalHeader>Invite New Employee</ModalHeader>
-//                         <ModalCloseButton />
-//                         <ModalBody>
-//                             <div style={{ display: 'flex', flexDirection: 'column' }}>
-//                                 <div style={{ display: 'flex', justifyContent: 'space-between', margin: '20px' }}>
-//                                     <TextField id="standard-basic" value={state.first_name} label="Employee Name" variant="standard" onChange={handleChange} name="employee_name" required fullWidth />
-
-//                                 </div>
-//                                 <div style={{ display: 'flex', justifyContent: 'space-between', margin: '20px' }}>
-
-//                                     <TextField id="standard-basic" value={state.designation} label="Designation" variant="standard" onChange={handleChange} name="designation" required />
-//                                     <TextField id="standard-basic" value={state.department} label="Department" variant="standard" sx={{ width: '200px' }} onChange={handleChange} name="department" required />
-//                                 </div>
-//                                 <div style={{ display: 'flex', justifyContent: 'space-between', margin: '20px' }}>
-//                                     <TextField id="standard-basic" label="Email Address" name="email" type='email' variant="standard" value={state.email} required fullWidth onChange={handleChange} />
-//                                 </div>
-//                             </div>
-
-//                         </ModalBody>
-
-//                         <ModalFooter>
-//                             <Button colorScheme='blue' mr={3} type="submit">
-//                                 Invite
-//                             </Button>
-//                         </ModalFooter>
-
-//                     </form>
-//                 </ModalContent>
-//             </Modal>
-//         </div>
-//     )
-// }
-
-// export default Presentation
 function Presentation(props) {
     const { handleChange, state } = props
     return (
